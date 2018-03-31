41a89205e00a4c2f2261521bd5c243fdb0382ced
Unknown <CrazyGrape@protonmail.com>
2018-03-31 02:45:46 -0600

    mcmod.info and versioning
    Will be 1.0.0 when officially released, with every 0.1 version increment for major feature additions to the mod

--- src/main/java/com/sandvoxel/immersivemagic/Refrence.java
+++ src/main/java/com/sandvoxel/immersivemagic/Refrence.java
@@ -2,8 +2,8 @@ package com.sandvoxel.immersivemagic;
 
 public class Refrence {
     public static final String MOD_ID = "immmag";
-    public static final String MOD_NAME = "ImmersiveMagic";
-    public static final String MOD_VERSION = "1.0.0";
+    public static final String MOD_NAME = "Immersive Magic";
+    public static final String MOD_VERSION = "0.1.0";
 
 
 }
