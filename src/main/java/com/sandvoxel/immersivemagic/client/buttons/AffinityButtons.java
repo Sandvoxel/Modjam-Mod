198be792ba4964b08d0dbcae91604fcb9f33138c
Sandvoxel <kovalchickzz@gmail.com>
2018-04-06 18:34:26 -0500

    fixed gui

--- src/main/java/com/sandvoxel/immersivemagic/client/buttons/AffinityButtons.java
+++ src/main/java/com/sandvoxel/immersivemagic/client/buttons/AffinityButtons.java
@@ -2,6 +2,7 @@ package com.sandvoxel.immersivemagic.client.buttons;
 
 import com.sandvoxel.immersivemagic.Reference;
 import com.sandvoxel.immersivemagic.api.magic.IAffinities;
+import com.sandvoxel.immersivemagic.common.magicdata.AffinitiesProvider;
 import com.sandvoxel.immersivemagic.common.magicdata.AffinityTypes;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
@@ -17,79 +18,40 @@ public class AffinityButtons extends GuiButton {
     final ResourceLocation texture = new ResourceLocation(Reference.MOD_ID,"textures/gui/affinity_list.png");
     private float texXStart = 0f;
     private float texYStart = 0f;
-    protected IAffinities affinities;
 
-    public AffinityButtons(int buttonId, int x, int y, EntityPlayer player, IAffinities affinities) {
+    public AffinityButtons(int buttonId, int x, int y, EntityPlayer player) {
         super(buttonId, x, y, "");
         id = buttonId;
         width = 18;
         height =18;
-        visible = true;
-        this.affinities = affinities;
+        visible = false;
         this.player = player;
     }
 
-    public int getButtonID() {
-        return id;
-    }
-
-    public IAffinities getAffinityInfo() {
-        return affinities;
-    }
 
     @Override
     public void drawButton(Minecraft mc, int mouseX, int mouseY, float partialTicks) {
-        if (this.visible)
+        IAffinities affinities = player.getCapability(AffinitiesProvider.AFFINITIES_CAPABILITY,null);
+
+        mc.getTextureManager().bindTexture(texture);
+
+        this.hovered = mouseX >= this.x && mouseY >= this.y && mouseX < this.x + this.width && mouseY < this.y + this.height;
+
+        texXStart = (float)id * 18.0f;
+
+        texYStart = 0.0f;
+        if(affinities.hasAffinity(AffinityTypes.getAffinity(id))){
+            texYStart = 18.0f;
+        } else {
+            texYStart = 36.0f;
+        }if (this.hovered)
         {
-
-            FontRenderer fontrenderer = mc.fontRenderer;
-            mc.getTextureManager().bindTexture(texture);
-            GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
-            this.hovered = mouseX >= this.x && mouseY >= this.y && mouseX < this.x + this.width && mouseY < this.y + this.height;
-            int i = this.getHoverState(this.hovered);
-            GlStateManager.enableBlend();
-            GlStateManager.tryBlendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA, GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO);
-            GlStateManager.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
-            texXStart = (float)id * 18.0f;
-            //this.drawTexturedModalRect(this.x, this.y, texXStart, texYStart, this.width, this.height);
-            //this.drawTexturedModalRect(this.x + this.width, this.y, texXStart, texYStart, this.width, this.height);
-            this.mouseDragged(mc, mouseX, mouseY);
-            int j = 14737632;
-
-        //Logic for how the button should render if the player has the affinity
-            texYStart = 0.0f;
-            if(affinities.hasAffinity(AffinityTypes.getAffinity(id))){
-                texYStart = 18.0f;
-            } else {
-                texYStart = 36.0f;
-            }
-        //Other button logic
-            if (packedFGColour != 0)
-            {
-                j = packedFGColour;
-                texYStart = 0.0f;
-            }
-            else
-            if (!this.enabled)
-            {
-                j = 10526880;
-                texYStart = 0.0f;
-            }
-            else if (this.hovered)
-            {
-
-                j = 16777120;
-                texYStart = 54.0f;
-            } else {
-
-            }
-
-
-            //Actual drawing of the texture
-            drawModalRectWithCustomSizedTexture(this.x, this.y, texXStart, texYStart, this.width, this.height, 144.0f, 72.0f);
-
-            this.drawCenteredString(fontrenderer, this.displayString, this.x + this.width / 2, this.y + (this.height - 8) / 2, j);
+            texYStart = 54.0f;
         }
+
+        drawModalRectWithCustomSizedTexture(this.x, this.y, texXStart, texYStart, this.width, this.height, 144.0f, 72.0f);
+
+        super.drawButton(mc, mouseX, mouseY, partialTicks);
     }
 
     @Override
